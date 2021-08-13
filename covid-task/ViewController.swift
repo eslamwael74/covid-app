80e798e0177b9243a7a5c494f2f7ee22df169b84
EslamWael <eslam.wael@bites.com>
2021-08-13 17:03:14 +0200

    Initial Commit

---
+++ covid-task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-task
+//
+//  Created by Eslam Wael on 8/13/21.
+//  Copyright © 2021 Eslam Wael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
